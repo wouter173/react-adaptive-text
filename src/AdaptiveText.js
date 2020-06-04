ab179a4ddf49167d011350506b43b5c8c879d903
Maxime Duyck <m.duyck@holidaypirates.com>
2020-06-04 17:58:14 +0200

    remove propTypes dependencies from project

--- src/AdaptiveText.js
+++ src/AdaptiveText.js
@@ -3,7 +3,6 @@ import React, {
   useRef,
   useState,
 } from 'react';
-import PropTypes from 'prop-types';
 import { useResize } from './useResize.js';
 
 const AdaptiveText = ({
@@ -37,7 +36,7 @@ const AdaptiveText = ({
       wrapperStyle[propName] = styleProp;
     }
   };
-
+  
   addStyle(color, 'color');
   addStyle(fontFamily, 'fontFamily');
   addStyle(fontStyle, 'fontStyle');
@@ -46,25 +45,29 @@ const AdaptiveText = ({
   addStyle(width, 'width');
   
   useEffect(() => {
-    const fontSizeRefPx = window.getComputedStyle(textEl.current).getPropertyValue('font-size');
-    const fontSizeRef = parseFloat(fontSizeRefPx);
-    const textWidthRef = textEl.current.offsetWidth === 0 ? 1 : textEl.current.offsetWidth;
-    setFontSizeRef(fontSizeRef);
-    setTextWidthRef(textWidthRef);
-    textEl.current.style.whiteSpace = null;
-    setTextStyle({
-      fontSize: getFontSize(text, fontSizeRef, textWidthRef, fontSizeMax, fontSizeMin),
-    });
+    if (!text) {
+      console.error('AdaptiveText: Prop \'text\' is required');
+    } else {
+      const fontSizeRefPx = window.getComputedStyle(textEl.current).getPropertyValue('font-size');
+      const fontSizeRef = parseFloat(fontSizeRefPx);
+      const textWidthRef = textEl.current.offsetWidth === 0 ? 1 : textEl.current.offsetWidth;
+      setFontSizeRef(fontSizeRef);
+      setTextWidthRef(textWidthRef);
+      textEl.current.style.whiteSpace = null;
+      setTextStyle({
+        fontSize: getFontSize(text, fontSizeRef, textWidthRef, fontSizeMax, fontSizeMin),
+      });
+    }
   }, [fontSizeMax, fontSizeMin, text, width]);
-
+  
   useResize(() => setTextStyle({
     fontSize: getFontSize(text, fontSizeRef, textWidthRef, fontSizeMax, fontSizeMin),
   }));
-
+  
   const getFontSize = (text, fontSizeRef, textWidthRef, fontSizeMax, fontSizeMin) => {
     textEl.current.style.display = 'none';
     const wrapperWidth = wrapperEl.current.clientWidth;
-    if (text.length <= 0) {
+    if (!text || text.length <= 0) {
       return 0;
     }
     const fontSize = (fontSizeRef * wrapperWidth / textWidthRef) - 1;
@@ -83,18 +86,6 @@ const AdaptiveText = ({
       <span ref={textEl} style={textStyle}>{text}</span>
     </span>
   );
-};
-
-AdaptiveText.propTypes = {
-  color: PropTypes.string,
-  fontFamily: PropTypes.string,
-  fontSizeMax: PropTypes.number,
-  fontSizeMin: PropTypes.number,
-  fontStyle: PropTypes.string,
-  fontWeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  text: PropTypes.string.isRequired,
-  textDecoration: PropTypes.string,
-  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
 
 export default AdaptiveText;
